26d8d929eb73ae36486e25c0044985bc5eae8eab
SkyrimForest <hitskyrim@qq.com>
2025-11-03 09:55:13 +0800

    feat: update markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,19 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Photographer',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/skyrimforest/Photographer.git' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
-						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide233', slug: 'guides/example' },
+						{ label: '阶段00课程大纲', slug: 'guides/outline' },
+						{ label: '阶段01技术基础', slug: 'guides/outline' },
+						{ label: '阶段02构图基础', slug: 'guides/outline' },
+						{ label: '阶段03光影风格', slug: 'guides/outline' },
+						{ label: '阶段04色彩表达', slug: 'guides/outline' },
+						{ label: '阶段05个人风格', slug: 'guides/outline' },
 					],
 				},
 				{
